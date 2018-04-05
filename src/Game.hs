6064d54ed86689b6c32a0a20078145b718baf6e2
kk <krishnakarthik1309@gmail.com>
2018-04-05 17:06:13 +0530

    add drawing dashes method

--- src/Game.hs
+++ src/Game.hs
@@ -6,8 +6,9 @@ data Player = Player1 | Player2 deriving (Eq, Show)
 data State = Running | GameOver (Maybe Player) deriving (Eq, Show)
 data Dot = Selected | NotSelected deriving (Eq, Show)
 
-type Board = Array (Int, Int) Dot
+type Board = Array ((Int, Int), (Int, Int)) Dash
 type Box = Maybe Player
+type Dash = Maybe Player
 
 data Game = Game { gameBoard :: Board
                  , gamePlayer :: Player
@@ -25,14 +26,14 @@ screenHeight :: Int
 screenHeight = 480
 
 boxWidth :: Float
-boxWidth = fromIntegral screenWidth / fromIntegral (n + 1)
+boxWidth = fromIntegral (min screenHeight screenWidth) / fromIntegral (n + 1)
 
 boxHeight :: Float
-boxHeight = fromIntegral screenHeight / fromIntegral (n + 1)
+boxHeight = fromIntegral (min screenHeight screenWidth) / fromIntegral (n + 1)
 
 -- Game
-initialGame = Game { gameBoard = array indexRange $ zip (range indexRange) (cycle [NotSelected])
+initialGame = Game { gameBoard = array indexRange $ zip (range indexRange) (cycle [Nothing])]
                    , gamePlayer = Player1
                    , gameState = Running
                    }
-                   where indexRange = ((0, 0), (n-1, n-1))
+                   where indexRange = (((0, 0), (0, 0)), ((n-1, n-1), (n-1, n-1)))
--- src/Rendering.hs
+++ src/Rendering.hs
@@ -1,5 +1,7 @@
 module Rendering where
 
+import Data.Array
+
 import Graphics.Gloss
 
 import Game
@@ -9,6 +11,32 @@ boardAsRunningPicture board = Blank
 outcomeColor (Just Player1) = makeColorI 255 50 50 255     -- red
 outcomeColor (Just Player2) = makeColorI 50 100 255 255    -- blue
 outcomeColor Nothing = greyN 0.5
+
+translateDash picture dot1Row dot1Column ex ey =
+    translate x y picture
+    where x = fromIntegral dot1Column * boxWidth + boxWidth * ex
+          y = fromIntegral dot1Row * boxHeight + boxHeight * ey
+
+snapPictureToDash picture ((dot1Row, dot1Column), (dot2Row, dot2Column)) =
+    if dot1Row == dot2Row
+        then translateDash picture dot1Row dot1Column 0.5 0
+        else translateDash (rotate 90 picture) dot1Row dot1Column 0 0.5
+
+linePicture :: Picture
+linePicture = pictures [rectangleSolid boxWidth 5.0]
+
+dashesOfBoard :: Board -> Dash -> Picture -> Picture
+dashesOfBoard board dash dashPicture =
+    pictures
+    $ map (snapPictureToDash dashPicture . fst)
+    $ filter (\(_, e) -> e == dash)
+    $ assocs board
+
+player1Dashes :: Board -> Picture
+player1Dashes board = dashesOfBoard board (Just Player1) linePicture
+
+player2Dashes :: Board -> Picture
+player2Dashes board = dashesOfBoard board (Just Player1) linePicture
 
 drawRowDots rowNum = concatMap (\i -> [ translate (i * boxWidth) (rowNum * boxHeight) (thickCircle 1.0 2.0)
                               ])
@@ -20,9 +48,10 @@ dotsOfBoard =
     $ concatMap (\i -> drawRowDots i)
      [0 .. fromIntegral (n - 1)]
 
--- #TODO: draw lines
 boardAsPicture board =
-    pictures [ dotsOfBoard
+    pictures [ dotsOfBoard,
+               player1Dashes board,
+               player2Dashes board
              ]
 
 boardAsGameOverPicture winner board =
